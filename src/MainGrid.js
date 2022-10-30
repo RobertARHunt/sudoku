839bdc15d5aa3d1c585370467fc5745f47cecc85
Robert Hunt <robertarhunt@gmail.com>
2022-10-30 10:48:47 +0000

    begun preparing a folder for solve functions and refactored every helper function into helpers.js

--- src/MainGrid.js
+++ src/MainGrid.js
@@ -2,83 +2,16 @@ import styled from 'styled-components';
 import { useState } from 'react';
 import GridCell from './GridCell';
 import EXAMPLES from './EXAMPLES';
-
-function getEmptyCell(props) {
-  return {
-    ...props,
-    value: 0,
-    options: new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]),
-  };
-}
-
-function generateEmptyGrid() {
-  const cells = [];
-  for (let row = 0; row < 9; row++) {
-    for (let column = 0; column < 9; column++) {
-      const segment = Math.floor(column / 3) + 3 * Math.floor(row / 3);
-      const newCell = getEmptyCell({
-        row,
-        column,
-        segment,
-        checkered: segment % 2 === 0,
-      });
-      cells.push(newCell);
-    }
-  }
-
-  return cells;
-}
-
-function generateFromExample(example) {
-  const cells = generateEmptyGrid();
-  example?.split('')?.forEach((v, i) => (cells[i].value = Number(v)));
-  const cellsWithNewOptions = cells.map((original) =>
-    setCellOptions(original, cells)
-  );
-  return cellsWithNewOptions;
-}
-
-function getStartState(example) {
-  return {
-    cells: generateFromExample(example),
-  };
-}
-
-function setCellValue(cell, newValue) {
-  return { ...cell, value: newValue };
-}
-
-function setCellValueInGrid(cell, newValue, cells) {
-  const cellsWithNewValue = cells.map((original) =>
-    cell === original ? setCellValue(original, newValue) : original
-  );
-  const siblingCells = getCellSiblings(cell, cells);
-  const cellsWithNewOptions = cellsWithNewValue.map((original) =>
-    siblingCells.includes(original)
-      ? setCellOptions(original, cellsWithNewValue)
-      : original
-  );
-  return cellsWithNewOptions;
-}
-
-function setCellOptions(cell, cells) {
-  if (cell.value) {
-    return { ...cell, options: new Set() };
-  }
-  const siblingCells = getCellSiblings(cell, cells);
-  const options = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-  siblingCells.forEach((c) => options.delete(c.value));
-  return { ...cell, options };
-}
-
-function getCellSiblings(cell, cells) {
-  return cells.filter(
-    (c) =>
-      c.row === cell.row ||
-      c.column === cell.column ||
-      c.segment === cell.segment
-  );
-}
+import {
+  getEmptyCell,
+  generateEmptyGrid,
+  generateFromExample,
+  getStartState,
+  setCellValue,
+  setCellValueInGrid,
+  setCellOptions,
+  getCellSiblings,
+} from './helpers';
 
 function MainGrid({ selectedNumber }) {
   const [gridState, setGridState] = useState(
---
+++ src/helpers.js
@@ -0,0 +1,120 @@
+function onlyUnique() {}
+
+function removeOptionsFromCells() {}
+
+function removeOptionsFromGroup() {}
+
+function optionsExcept() {}
+
+function mergedCellOptions() {}
+
+function getCellValue() {}
+
+function deleteCellOption() {}
+
+function setCellOptions(cell, cells) {
+  if (cell.value) {
+    return { ...cell, options: new Set() };
+  }
+  const siblingCells = getCellSiblings(cell, cells);
+  const options = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  siblingCells.forEach((c) => options.delete(c.value));
+  return { ...cell, options };
+}
+
+function setCellValue(cell, newValue) {
+  return { ...cell, value: newValue };
+}
+
+function getCellSiblings(cell, cells) {
+  return cells.filter(
+    (c) =>
+      c.row === cell.row ||
+      c.column === cell.column ||
+      c.segment === cell.segment
+  );
+}
+
+function setCellValueInGrid(cell, newValue, cells) {
+  const cellsWithNewValue = cells.map((original) =>
+    cell === original ? setCellValue(original, newValue) : original
+  );
+  const siblingCells = getCellSiblings(cell, cells);
+  const cellsWithNewOptions = cellsWithNewValue.map((original) =>
+    siblingCells.includes(original)
+      ? setCellOptions(original, cellsWithNewValue)
+      : original
+  );
+  return cellsWithNewOptions;
+}
+
+function getEmptyCell(props) {
+  return {
+    ...props,
+    value: 0,
+    options: new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+  };
+}
+
+function generateEmptyGrid() {
+  const cells = [];
+  for (let row = 0; row < 9; row++) {
+    for (let column = 0; column < 9; column++) {
+      const segment = Math.floor(column / 3) + 3 * Math.floor(row / 3);
+      const newCell = getEmptyCell({
+        row,
+        column,
+        segment,
+        checkered: segment % 2 === 0,
+      });
+      cells.push(newCell);
+    }
+  }
+
+  return cells;
+}
+
+function generateFromExample(example) {
+  const cells = generateEmptyGrid();
+  example?.split('')?.forEach((v, i) => (cells[i].value = Number(v)));
+  const cellsWithNewOptions = cells.map((original) =>
+    setCellOptions(original, cells)
+  );
+  return cellsWithNewOptions;
+}
+
+function getStartState(example) {
+  return {
+    cells: generateFromExample(example),
+  };
+}
+
+function getCombinations(array, size) {
+  if (array.length < size) return [];
+
+  const result = [];
+
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    if (size == 1) {
+      result.push([element]);
+    } else {
+      const remaining = array.slice(index + 1);
+      const next = getCombinations(remaining, size - 1);
+      next.forEach((n) => result.push([element, ...n]));
+    }
+  }
+
+  return result;
+}
+
+export {
+  getEmptyCell,
+  generateEmptyGrid,
+  generateFromExample,
+  getStartState,
+  setCellValue,
+  setCellValueInGrid,
+  setCellOptions,
+  getCellSiblings,
+};
